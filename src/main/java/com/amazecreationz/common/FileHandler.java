ed9c3ea881318428679f13d4791054fbf7e5b937
Anand Mohan <anandmoghan@amazecreationz.in>
2016-12-03 20:41:38 +0530

    Updated

--- src/main/java/com/amazecreationz/common/FileHandler.java
+++ src/main/java/com/amazecreationz/common/FileHandler.java
@@ -20,10 +20,30 @@ public class FileHandler {
 	}
 	
 	protected boolean createFile(String fileName){
-    	return true;
+		File file = new File(DATA_DIR +"/" +fileName);
+		try {
+			if(file.createNewFile())
+				return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	return false;
+    }
+	
+	protected boolean createDir(String dirName){
+		File file = new File(DATA_DIR +"/" +dirName);
+		try {
+			if(!file.exists()){
+				file.mkdirs();
+				return true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	return false;
     }
     
-    protected boolean uploadFile(HttpServletRequest request){
+	public boolean uploadFile(HttpServletRequest request){
     	if(ServletFileUpload.isMultipartContent(request)){
 			try {
 				List<FileItem> multiparts = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
@@ -41,7 +61,7 @@ public class FileHandler {
     	return false;
     }
     
-    protected HttpServletResponse downloadFile(String fileName, HttpServletResponse response){
+	public HttpServletResponse downloadFile(String fileName, HttpServletResponse response){
     	try {
 			File file = new File(DATA_DIR +"/" +fileName);
 			response.setContentType("application/octet-stream");
@@ -62,15 +82,32 @@ public class FileHandler {
     	return response;
     }
     
-    protected boolean moveFile(String fileName, String location){
-    	return true;
+	public boolean moveFile(String fileName, String location){
+		File file = new File(DATA_DIR +"/" +fileName);
+		if(file.renameTo(new File(location +"/" +fileName)))
+			return true;
+		else
+			return false;
     }
     
-    protected boolean renameFile(String fileName, String newFileName){
-    	return true;
+	public boolean renameFile(String fileName, String newFileName){
+		File file1 = new File(DATA_DIR +"/" +fileName);
+		File file2 = new File(DATA_DIR +"/" +newFileName);
+		if (!file2.exists()){
+			file1.renameTo(file2);
+			return true;
+		}
+    	return false;
     }
     
-    protected boolean removeFile(String fileName){
-    	return true;
+	public boolean removeFile(String fileName){
+		File file = new File(DATA_DIR +"/" +fileName);
+		try {
+			if(file.delete())
+				return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	return false;
     }
 }
